112490f69508dccf77431c5896e6a1fc03c34598
Markus Korpinen <markus.korpinen@gmail.com>
2013-02-24 17:22:10 +0200

    tiedostojen lisäys versiohallintaan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsohar::Application.config.secret_token = 'c306d1f7fae6537c996e534bdd836fbc2bb02a67193fbd46ff9f332a2a045e2301c131417ea0c61ebbef019298e7f5cb68939a459dafb4b27f5509d95b423851'
